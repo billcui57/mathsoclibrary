a503e4b9f1cdf41a399b1268b299b986a8c7dedd
Bill Cui <billcui2001@hotmail.com>
2020-04-21 17:27:00 -0400

    updates to deploy to heroku

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase: {
+    apiKey: "AIzaSyBAGX6CXQG8DodCL-AB_ck-HssTGhm6PFA",
+    authDomain: "mathsoclibrary.firebaseapp.com",
+    databaseURL: "https://mathsoclibrary.firebaseio.com",
+    projectId: "mathsoclibrary",
+    storageBucket: "mathsoclibrary.appspot.com",
+    messagingSenderId: "131028391526",
+  }
 };
