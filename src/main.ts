d6f36349b6d5f1581a2f81589e10546c77cb69db
Bill Cui <billcui2001@hotmail.com>
2020-04-27 18:31:28 -0400

    Safari Support

--- src/main.ts
+++ src/main.ts
@@ -4,11 +4,13 @@ import { platformBrowserDynamic } from '@angular/platform-browser-dynamic';
 import { AppModule } from './app/app.module';
 import { environment } from './environments/environment';
 
-// polyfill TextEncoder for IE Edge
-import { TextEncoder } from 'text-encoding';
-if (typeof (window as any).TextEncoder === 'undefined') {
-  (window as any).TextEncoder = TextEncoder;
-}
+  // polyfill TextEncoder for IE Edge 
+  // I dont know why this can't be placed in polyfills.ts but the login system doesnt work if it is placed in there
+  // LOL what
+  import { TextEncoder } from 'text-encoding';
+  if (typeof (window as any).TextEncoder === 'undefined') {
+    (window as any).TextEncoder = TextEncoder;
+  }
 
 if (environment.production) {
   enableProdMode();
@@ -16,3 +18,4 @@ if (environment.production) {
 
 platformBrowserDynamic().bootstrapModule(AppModule)
   .catch(err => console.error(err));
+
